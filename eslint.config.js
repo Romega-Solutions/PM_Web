2f1fec50a5c8270f6310a5003e941a93f5f5d250
Mark Siazon <67873853+Iron-Mark@users.noreply.github.com>
2026-07-01 03:43:39 +0800

    chore(deps): combine PM_Web dependency updates

--- eslint.config.js
+++ eslint.config.js
@@ -1,23 +1,34 @@
 import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
+import { reactRefresh } from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 import { globalIgnores } from 'eslint/config'
 
-export default tseslint.config([
+const { configs: _typescriptConfigs, ...typescriptPlugin } = tseslint.plugin
+const { configs: _reactHooksConfigs, ...reactHooksPlugin } = reactHooks
+const reactRefreshViteConfig = reactRefresh.configs.vite()
+
+export default [
   globalIgnores(['dist']),
   {
     files: ['**/*.{ts,tsx}'],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs['recommended-latest'],
-      reactRefresh.configs.vite,
-    ],
     languageOptions: {
+      parser: tseslint.parser,
       ecmaVersion: 2020,
       globals: globals.browser,
     },
+    plugins: {
+      '@typescript-eslint': typescriptPlugin,
+      'react-hooks': reactHooksPlugin,
+      ...reactRefreshViteConfig.plugins,
+    },
+    rules: {
+      ...js.configs.recommended.rules,
+      ...tseslint.configs.recommended[1].rules,
+      ...tseslint.configs.recommended[2].rules,
+      ...reactHooks.configs.flat['recommended-latest'].rules,
+      ...reactRefreshViteConfig.rules,
+    },
   },
-])
+]
